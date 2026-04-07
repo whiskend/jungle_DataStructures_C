a644e2f23bcb3e528b7424507b9d9629ef0f360f
이경근 <lsg008@naver.com>
2026-04-08 00:30:39 +0900

    Q1_E_BT.c 풀이

--- Binary_Tree/Q1_E_BT.c
+++ Binary_Tree/Q1_E_BT.c
@@ -117,6 +117,14 @@ int identical(BTNode *tree1, BTNode *tree2)
 
 {
    /* add your code here */
+   if(tree1==NULL && tree2==NULL) return 1;
+   else if((tree1==NULL || tree2==NULL)) return 0;
+   else if(tree1->item != tree2->item) return 0;
+
+   if(identical(tree1->left, tree2->left) && identical(tree1->right, tree2->right)) return 1;
+   else return 0;
+
+   return 0;
 }
 
 /////////////////////////////////////////////////////////////////////////////////
